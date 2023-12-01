b0c6736434930ebbbaa40dcb5c0eb822a1abbfe1
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-12-01 19:42:19 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Preparar...\n");
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.ReadKey();
+Console.WriteLine("Apontar...\n");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.ReadKey();
+Console.WriteLine("FOGO!");
